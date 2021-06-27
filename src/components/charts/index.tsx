7f689dc9cb0ec3fabf997c090bd3dc821144d74f
montsouz <guilhermesouzamont@gmail.com>
2021-06-27 20:38:35 -0300

    feat(charts): adding charts

---
+++ src/components/charts/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+
+interface DoughnutChartProps {
+  label: string;
+  labels: string[];
+  chartData: number[];
+}
+
+const DoughnutChart: React.FC<DoughnutChartProps> = ({
+  labels,
+  label,
+  chartData,
+}) => {
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label,
+        data: chartData,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Doughnut type="doughnut" data={data} options={options} />;
+};
+
+export { DoughnutChart };
--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Intent, Message } from "src/model";
+import { DoughnutChart } from "src/components/charts";
 import "./styles.css";
+
+const NOT_ANSWERED_INTENT = "nao-respondido";
 
 interface DashboardContainerProps {
   intents: Intent[];
@@ -15,6 +18,11 @@ interface SelectIntentsProps {
 
 interface ListMessagesProps {
   messages: Message[];
+}
+
+interface DataChart {
+  labels: string[];
+  chartData: number[];
 }
 
 const SelectIntent: React.FC<SelectIntentsProps> = ({ items, onSelect }) => (
@@ -50,13 +58,68 @@ const ListMessages: React.FC<ListMessagesProps> = ({ messages }) => {
   );
 };
 
+const getIntentsCounter = (intents: Intent[]): DataChart => {
+  const data: DataChart = {
+    labels: [],
+    chartData: [],
+  };
+
+  return intents.reduce((acc, curr) => {
+    acc.labels.push(curr.name);
+    acc.chartData.push(curr.counter);
+    return acc;
+  }, data);
+};
+
+const getAnsweredRate = (intents: Intent[]): DataChart => {
+  const data: DataChart = {
+    labels: ["Não Respondidas", "Respondidas"],
+    chartData: [],
+  };
+
+  let answeredCount = 0;
+
+  for (const intent of intents) {
+    if (intent.intent === NOT_ANSWERED_INTENT) {
+      data.chartData.push(intent.counter);
+      break;
+    }
+
+    answeredCount += intent.counter;
+  }
+
+  data.chartData.push(answeredCount);
+  return data;
+};
+
 const DashboardContainer: React.FC<DashboardContainerProps> = ({
   intents,
   messages,
   onSelect,
 }) => {
+  const intentsCounterData = getIntentsCounter(intents);
+  const answeredRate = getAnsweredRate(intents);
   return (
     <div className="dashboard-container">
+      <h2>Estatisticas</h2>
+
+      <div className="charts-container">
+        <div className="chart-container">
+          <DoughnutChart
+            label="Intents counter"
+            labels={intentsCounterData.labels}
+            chartData={intentsCounterData.chartData}
+          />
+        </div>
+        <div className="chart-container">
+          <DoughnutChart
+            label="Answers counter"
+            labels={answeredRate.labels}
+            chartData={answeredRate.chartData}
+          />
+        </div>
+      </div>
+
       <h2>Intenções</h2>
       <SelectIntent items={intents} onSelect={onSelect} />
       <ListMessages messages={messages} />
