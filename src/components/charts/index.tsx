3d66ad3695336aa4b97355676a8011e3f2f29b01
montsouz <guilhermesouzamont@gmail.com>
2021-06-28 10:14:46 -0300

    feat: list feedback stats

--- src/components/charts/index.tsx
+++ src/components/charts/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Bar } from "react-chartjs-2";
 
 interface DoughnutChartProps {
   label: string;
@@ -15,7 +15,7 @@ const DoughnutChart: React.FC<DoughnutChartProps> = ({
   const options = {
     plugins: {
       legend: {
-        display: false,
+        display: true,
       },
     },
   };
@@ -50,4 +50,47 @@ const DoughnutChart: React.FC<DoughnutChartProps> = ({
   return <Doughnut type="doughnut" data={data} options={options} />;
 };
 
-export { DoughnutChart };
+const BarChart: React.FC<DoughnutChartProps> = ({
+  labels,
+  label,
+  chartData,
+}) => {
+  const options = {
+    plugins: {
+      legend: {
+        display: true,
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label,
+        data: chartData,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Bar type="bar" data={data} options={options} />;
+};
+
+export { DoughnutChart, BarChart };
--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Intent, Message } from "src/model";
-import { DoughnutChart } from "src/components/charts";
+import { DoughnutChart, BarChart } from "src/components/charts";
 import "./styles.css";
 
 const NOT_ANSWERED_INTENT = "nao-respondido";
+
+const FEEDBACK_INTENTS = ["nao-consegui-ajudar", "conseguiu-ajudar"];
 
 interface DashboardContainerProps {
   intents: Intent[];
@@ -20,9 +22,18 @@ interface ListMessagesProps {
   messages: Message[];
 }
 
+interface BlocksProps {
+  data: CustomChart[];
+}
+
 interface DataChart {
   labels: string[];
   chartData: number[];
+}
+
+interface CustomChart {
+  label: string;
+  data: number;
 }
 
 const SelectIntent: React.FC<SelectIntentsProps> = ({ items, onSelect }) => (
@@ -55,6 +66,19 @@ const ListMessages: React.FC<ListMessagesProps> = ({ messages }) => {
     </div>
   ) : (
     <div>Chose a intent</div>
+  );
+};
+
+const Blocks: React.FC<BlocksProps> = ({ data }) => {
+  return (
+    <div className="blocks-container">
+      {data.map((d) => (
+        <div className="block">
+          <div className="block-label">{d.label}</div>
+          <div className="block-data">{d.data}</div>
+        </div>
+      ))}
+    </div>
   );
 };
 
@@ -92,6 +116,20 @@ const getAnsweredRate = (intents: Intent[]): DataChart => {
   return data;
 };
 
+const getNumberOfTimesThatHelped = (intents: Intent[]): CustomChart[] => {
+  const customChart: CustomChart[] = [];
+
+  return intents.reduce((acc, curr) => {
+    if (FEEDBACK_INTENTS.includes(curr.intent)) {
+      acc.push({
+        label: curr.name,
+        data: curr.counter,
+      });
+    }
+    return acc;
+  }, customChart);
+};
+
 const DashboardContainer: React.FC<DashboardContainerProps> = ({
   intents,
   messages,
@@ -99,30 +137,36 @@ const DashboardContainer: React.FC<DashboardContainerProps> = ({
 }) => {
   const intentsCounterData = getIntentsCounter(intents);
   const answeredRate = getAnsweredRate(intents);
+  const feedBackData = getNumberOfTimesThatHelped(intents);
   return (
     <div className="dashboard-container">
-      <h2>Estatisticas</h2>
-
-      <div className="charts-container">
-        <div className="chart-container">
-          <DoughnutChart
-            label="Intents counter"
-            labels={intentsCounterData.labels}
-            chartData={intentsCounterData.chartData}
-          />
-        </div>
-        <div className="chart-container">
-          <DoughnutChart
-            label="Answers counter"
-            labels={answeredRate.labels}
-            chartData={answeredRate.chartData}
-          />
+      <div style={{ width: "50%", marginRight: "50px" }}>
+        <h2>Intenções</h2>
+        <SelectIntent items={intents} onSelect={onSelect} />
+        <ListMessages messages={messages} />
+      </div>
+      <div>
+        <h2>Estatisticas</h2>
+        <div className="charts-container">
+          <div className="blocks-and-chart">
+            <Blocks data={feedBackData} />
+            <div className="chart-container">
+              <DoughnutChart
+                label="Answers counter"
+                labels={answeredRate.labels}
+                chartData={answeredRate.chartData}
+              />
+            </div>
+          </div>
+          <div className="big-chart-container">
+            <BarChart
+              label="Intents counter"
+              labels={intentsCounterData.labels}
+              chartData={intentsCounterData.chartData}
+            />
+          </div>
         </div>
       </div>
-
-      <h2>Intenções</h2>
-      <SelectIntent items={intents} onSelect={onSelect} />
-      <ListMessages messages={messages} />
     </div>
   );
 };
